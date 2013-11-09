112c20450d20f9bc9789847fe4d8f9ec389a7226
takaidohigasi <takaidohigasi@gmail.com>
2013-11-09 14:57:39 +0900

    デフォルトのディレクトリ等を追加

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://production.s3.rubygems.org'
+
+group :development, :test do
+  gem "rspec"
+  gem "yard"
+end
---
+++ default/spec/spec_helper.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+# rspec default settings
+# -------------------------------------------------
+RSpec.configure do |c|
+  c.fail_fast = true
+end
+
+# configure load path
+# -------------------------------------------------
+here = File.dirname(__FILE__)
+$:.unshift File.expand_path('../lib', here)
+$:.unshift File.dirname(__FILE__)
